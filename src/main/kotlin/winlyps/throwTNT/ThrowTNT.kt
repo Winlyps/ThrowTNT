440fa0effd054e827b7b222d71ce240e6a4ca63d
Winlypse <nts3@o2.pl>
2025-05-21 19:48:35 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/throwTNT/ThrowTNT.kt
@@ -0,0 +1,14 @@
+package winlyps.throwTNT
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ThrowTNT : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
